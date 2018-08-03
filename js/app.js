266edae320acf58b028c9691d2f2638e6f7ab3fb
J. Simplicio <jessicasimplicio1@gmail.com>
2018-08-03 17:56:48 -0300

    filtro de tipo implementado

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
+//
+
+/*let typeSelected = [];
+let type = '';
+
+restaurantes.forEach(restaurant => {
+	if (restaurant.type == 'vegana'){
+		//console.log(restaurant.name);
+		typeSelected.push(restaurant);
+	}
+});
+	console.log(typeSelected);*/
+
+
 // Add your JavaScript
 $(document).ready(function() {
-	$("#splash").fadeOut(4000);
+
+	//Faz o splash
+	setTimeout(function(){
+		$("#splash").fadeOut(500);	
+	}, 3000);
+	
 	setTimeout(function(){
 		$("#main").fadeIn("slow")
-	}, 5000)
+	}, 3000);
+  /*$('#main').delay('3000').fadeIn('slow');*/
+  /*$("#main").attr("display","flex");*/
+
+  //Filtra por tipo:
+  $('.filter-type').change(function(){
+  	var type = '';
+  	//type = $('.filter-type').val(); /*====> DÁ UNDEFINED!!! PQ???????*/
+  	type = $(this).val();
+  	console.log(type);
+  });
+
+
+
 })
+
+
